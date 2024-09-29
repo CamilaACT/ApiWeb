350128a121cf3a8edbadd3b0ed3da43b634a790a
CamilaPROCELEC <camilacabrera464@gmail.com>
2024-09-29 14:11:08 -0500

    Inicio Repositorio, validación Inicial

---
+++ ApiProyecto.model/proc/Usuario.cs
@@ -0,0 +1,51 @@
+﻿using ApiProyecto.model.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ApiProyecto.model.proc
+{
+    public class Usuario
+    {
+        public static Respuesta listaUsuarios(int us_id)
+        {
+
+            Respuesta res = new Respuesta() { CodigoError = 0, Message = "Sin Resultados", Result = null };
+
+            try
+            {
+                
+                    data.DAO.c_base_datos cb = new data.DAO.c_base_datos();
+                    System.Data.DataTable dt;
+                    string strCon = util.Conexion.Conexion.CadenaConexion();
+                    string[] vector = new string[1];
+                    cb.sp = "usp_Web_usuarios_B_usuarios";//poner el nombre correcto
+                    vector[0] = "@us_id,i," + us_id;
+                    dt = cb.consultar(vector, 1, strCon);
+                    res.CodigoError = cb.valo_erro;
+                    if (res.CodigoError == -1)
+                    {
+                        res.Message = "OK";
+                        res.Message = cb.valo_resp;
+                        res.Result = dt;
+                    }
+                    else
+                    {
+                        res.Message = "Que pena me da tu caso";
+                        res.Message = cb.valo_resp;
+                    }
+                
+            }
+            catch (Exception ex)
+            {
+                res.CodigoError = -100;
+                res.Message = "Error inesperado";
+                res.Message = ex.Message;
+            }
+            return res;
+        }
+    }
+}
---
+++ ApiProyecto/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using ApiProyecto.model.Response;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiProyecto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [ActionName("1_1")]
+        [Route("api/c_1_Login/1_1")]
+        public Respuesta m_1_Login_1_1([FromBody] int us_id)
+        {
+            Respuesta res = model.proc.Usuario.listaUsuarios(us_id);
+            return res;
+        }
+    }
+}
